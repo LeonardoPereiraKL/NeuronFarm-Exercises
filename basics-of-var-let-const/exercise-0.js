6a1972833b8652b65026242d490ee1570a43c25d
Leonardo Pereira <ldnpmarketing@gmail.com>
2023-02-14 01:22:53 +0000

    adding exercise 0

---
+++ basics-of-var-let-const/exercise-0.js
@@ -0,0 +1,14 @@
+var definition = {
+    //Permite reatribuição e a variável pode ser redeclarada ao longo do código.
+}
+
+let definition = {
+    //Permite reatribuição, mas não pode ser redeclarada.
+}
+
+const definition = {
+    //Não permite reatribuição e não pode ser redeclarada.
+}
+
+
+//note: definition must not be assigned to all the variables, this is for showcase purposes
