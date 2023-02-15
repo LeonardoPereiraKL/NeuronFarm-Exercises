ce1d4bb4854dfe3e4855669d76f40ead65fe5705
Leonardo Pereira <ldnpmarketing@gmail.com>
2023-02-15 16:32:17 +0000

    feat: adding exercise 1

---
+++ tipos-em-javascript-number/exercise-1.js
@@ -0,0 +1,10 @@
+/* Exercício 1:
+    Crie duas variáveis com o nome "a" e "b" e atribua valores numéricos.
+    Em seguida, crie uma terceira variável com o nome "soma" que receba a soma das duas variáveis anteriores e imprima o resultado na tela. */
+
+let a = 29
+let b = 1
+
+let soma = a + b
+
+console.log(soma)
